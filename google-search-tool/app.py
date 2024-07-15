bb69d1cfb4d3fe0256adaffa48cc943d8d9b264c
murtaza147 <109812861+murtaza147@users.noreply.github.com>
2024-07-15 13:33:14 -0400

    Update app.py

--- google-search-tool/app.py
+++ google-search-tool/app.py
@@ -2,8 +2,8 @@ import os
 from langchain_google_community import GoogleSearchAPIWrapper
 from langchain_core.tools import Tool
 
-os.environ["GOOGLE_CSE_ID"] = "863ea58776fd54c45"
-os.environ["GOOGLE_API_KEY"] = "AIzaSyAZTjVgHZ0daryBpy7AJnuB7D0gR7R-des"
+os.environ["GOOGLE_CSE_ID"] = "863ea...fd54c45"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyAZTjV...JnuB7D0gR7R-des"
 
 tool = Tool(
     name="google_search",
