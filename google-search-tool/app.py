b34d6a222977d2a43acdfbeb5bb94e9dac314221
murtaza147 <109812861+murtaza147@users.noreply.github.com>
2024-07-15 13:32:39 -0400

    Add files via upload

---
+++ google-search-tool/app.py
@@ -0,0 +1,15 @@
+import os
+from langchain_google_community import GoogleSearchAPIWrapper
+from langchain_core.tools import Tool
+
+os.environ["GOOGLE_CSE_ID"] = "863ea58776fd54c45"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyAZTjVgHZ0daryBpy7AJnuB7D0gR7R-des"
+
+tool = Tool(
+    name="google_search",
+    description="Search Google for recent results.",
+    func=GoogleSearchAPIWrapper(k=1).run,
+)
+
+query = input("Search for: ")
+print(tool.run(query))
